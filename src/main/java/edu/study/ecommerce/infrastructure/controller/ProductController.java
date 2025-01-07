0b529ed61ab4930daea7f12ea1c1046ec055d622
Saul Echeverri <shecheve@bancolombia.com.co>
2025-01-07 10:57:48 -0500

    ✨(feat): Get a product to modify - Add "th" of Thymeleaf to "tr" of show.html and create.html to iterate over products - Create the "editProduct" controller to product edit

--- src/main/java/edu/study/ecommerce/infrastructure/controller/ProductController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/ProductController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -55,6 +56,17 @@ public class ProductController {
         Iterable<Product> products = productService.getProductsByUser(user);
         model.addAttribute("products", products);
         return "admin/products/show";
+    }
+
+
+    /**
+     * Edit product
+     * @param id
+     */
+    @GetMapping("/edit/{id}")
+    public void editProduct(@PathVariable Integer id) {
+    Product product = productService.getProductById(id);
+        log.info("Producto obtenido: {}", product);
     }
 
 }
