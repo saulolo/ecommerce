f7b2473e1da51da7659d2c53c0ab633e5d9408bc
Saul Echeverri <shecheve@bancolombia.com.co>
2025-01-07 13:18:55 -0500

    ✨(feat): Show products for admin on home page - Update the "startup" controller to receive the "Model" class as a parameter and set the user ID - Update the "home.html" file to iterate with thymeleaf each product

--- src/main/java/edu/study/ecommerce/infrastructure/controller/AdminController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/AdminController.java
@@ -1,6 +1,10 @@
 package edu.study.ecommerce.infrastructure.controller;
 
+import edu.study.ecommerce.application.service.ProductService;
+import edu.study.ecommerce.domain.Product;
+import edu.study.ecommerce.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,12 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final ProductService productService;
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
     /**
-     * Home
-     * @return String
+     * Home page
+     * @return String admin/home.html
      */
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        User user = new User();
+        user.setId(1);
+        Iterable<Product> products = productService.getProductsByUser(user);
+        model.addAttribute("products", products);
         return "admin/home";
     }
 }
