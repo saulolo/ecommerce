12b5ca8a396c786df09bb60ac481f14d46447c0c
Saul Echeverri <shecheve@bancolombia.com.co>
2025-01-14 10:06:16 -0500

    ✨(feat): Mostrar los productos en la home del usuario y ver los detalles del producto - Modify the method to get all products in HomeController - Send the obtained products to the home view. - Print the products in the home view. - Copy the productdetail.html template - Add the fragments to the template - Create the method to send to the productdetail template - Show the product detail in the template

--- src/main/java/edu/study/ecommerce/infrastructure/controller/HomeController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/HomeController.java
@@ -1,6 +1,8 @@
 package edu.study.ecommerce.infrastructure.controller;
 
+import edu.study.ecommerce.application.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,12 +10,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
 
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
     /**
-     * Home page
-     * @return home page
+     * Displays the home page with the list of products.
+     *
+     * This method retrieves the list of products from the database and adds it to the model.
+     * The list of products is then displayed on the home page.
+     *
+     * @param model the model to which the list of products is added
+     * @return the name of the view to display
      */
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("products", productService.getProducts());
         return "home";
+    }
+
+    /**
+     * Displays the product detail page.
+     *
+     * This method displays the product detail page for the specified product.
+     *
+     * @return the name of the view to display
+     */
+    @GetMapping("/product-detail/{id}")
+    public String productDetail() {
+        return "user/productdetail";
     }
 }
