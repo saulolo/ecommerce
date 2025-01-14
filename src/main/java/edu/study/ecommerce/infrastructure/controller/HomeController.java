c0e4eec057078af79a18ff3733aa1b72933dcca3
Saul Echeverri <shecheve@bancolombia.com.co>
2025-01-14 10:54:16 -0500

    ✨(feat): Implement functionality to view product details - Modify the method to get all products in HomeController - Send the obtained products to the home view. - Print the products in the home view. - Copy the productdetail.html template - Add the fragments to the template - Create the method to send to the productdetail template - Show the product detail in the template

--- src/main/java/edu/study/ecommerce/infrastructure/controller/HomeController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/HomeController.java
@@ -1,19 +1,26 @@
 package edu.study.ecommerce.infrastructure.controller;
 
 import edu.study.ecommerce.application.service.ProductService;
+import edu.study.ecommerce.application.service.StockService;
+import edu.study.ecommerce.domain.Stock;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
     private final ProductService productService;
+    private final StockService stockService;
 
-    public HomeController(ProductService productService) {
+    public HomeController(ProductService productService, StockService stockService) {
         this.productService = productService;
+        this.stockService = stockService;
     }
 
 
@@ -32,15 +39,23 @@ public class HomeController {
         return "home";
     }
 
+
     /**
      * Displays the product detail page.
      *
-     * This method displays the product detail page for the specified product.
+     * This method retrieves the product with the specified ID from the database and adds it to the model.
+     * The product detail page is then displayed.
      *
+     * @param id the ID of the product to display
+     * @param model the model to which the product is added
      * @return the name of the view to display
      */
     @GetMapping("/product-detail/{id}")
-    public String productDetail() {
+    public String productDetail(@PathVariable Integer id, Model model) {
+        List<Stock> stocks = stockService.getStockByProduct(productService.getProductById(id));
+        Integer lastBalance = stocks.get(stocks.size() - 1).getBalance();
+        model.addAttribute("product", productService.getProductById(id));
+        model.addAttribute("stock", lastBalance);
         return "user/productdetail";
     }
 }
