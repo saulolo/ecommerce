fdba033d780c54c669c14b2ca619c8bf6bb866c2
Saul Echeverri <shecheve@bancolombia.com.co>
2025-02-24 13:16:24 -0500

    ✨(feat): Functionality to show the shopping cart - Create the method to show the shopping car - Point from the link view to the method and show by console - Copy the cart.html template in the User/Cart directory - Add the Namespace to use Thymeleaf tags and add the User Fragmet - Show the products in the cart.html view

--- src/main/java/edu/study/ecommerce/infrastructure/controller/CartController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/CartController.java
@@ -3,6 +3,8 @@ package edu.study.ecommerce.infrastructure.controller;
 import edu.study.ecommerce.application.service.CartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,14 +23,14 @@ public class CartController {
     }
 
     /**
-     * Handles the addition of a product to the shopping cart.
+     * Handles POST requests to add a product to the shopping cart.
      * Redirects to the home page after adding the product.
      *
-     * @param quantity the quantity of the product to add.
-     * @param idProduct the ID of the product to add.
-     * @param nameProduct the name of the product to add.
-     * @param price the price of the product to add.
-     * @return a redirect to the home page.
+     * @param quantity the quantity of the product to add
+     * @param idProduct the ID of the product to add
+     * @param nameProduct the name of the product to add
+     * @param price the price of the product to add
+     * @return a redirect to the home page
      */
     @PostMapping("/add-product")
     public String addProductCart(@RequestParam Integer quantity,
@@ -36,10 +38,33 @@ public class CartController {
                                  @RequestParam String nameProduct,
                                  @RequestParam BigDecimal price) {
         cartService.addItemCart(quantity, idProduct, nameProduct, price);
+        showCart();
+        return "redirect:/home";
+    }
+
+    /**
+     * Logs the items in the shopping cart for debugging or informational purposes.
+     * Iterates through the cart items and logs each item using the application's logger.
+     */
+    private void showCart() {
         cartService.getItemCarts().forEach(
                 itemCart -> log.info("Item cart: {}", itemCart)
         );
-        return "redirect:/home";
+    }
+
+    /**
+     * Handles GET requests to display the shopping cart.
+     * Adds the cart items and total price to the model for rendering the view.
+     *
+     * @param model the model to which attributes are added for rendering the view
+     * @return the name of the view to be rendered
+     */
+    @GetMapping("/get-cart")
+    public String getCart(Model model) {
+        showCart();
+        model.addAttribute("cart", cartService.getItemCarts());
+        model.addAttribute("total", cartService.getTotalCart());
+        return "user/cart/cart";
     }
 
 }
