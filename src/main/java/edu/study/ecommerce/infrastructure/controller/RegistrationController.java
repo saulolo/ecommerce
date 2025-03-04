431281fd640ccb79c6d7258544332f95702348b3
Saul Echeverri <shecheve@bancolombia.com.co>
2025-03-04 11:48:57 -0500

    ✨(feat): Implement functionality for self-registration - Create the createuser method for user registration - Update the names of the HTML of Register.html elements to those of the User class - Register the user on database - Validate the data in database

--- src/main/java/edu/study/ecommerce/infrastructure/controller/RegistrationController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/RegistrationController.java
@@ -1,12 +1,26 @@
 package edu.study.ecommerce.infrastructure.controller;
 
+import edu.study.ecommerce.application.service.RegistrationService;
+import edu.study.ecommerce.domain.User;
+import edu.study.ecommerce.domain.UserType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
+
+
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
 
     /**
      * Handles GET requests for the registration page.
@@ -16,5 +30,22 @@ public class RegistrationController {
     @GetMapping
     public String register() {
         return "register";
+    }
+
+    /**
+     * Handles POST requests for the registration page.
+     * This method is called when a user submits the registration form.
+     * It creates a new user object and delegates the registration to the registration service.
+     *
+     * @param user the user object to be registered. Must not be null.
+     * @return The name of the view "register".
+     */
+    @PostMapping
+    public String registerUser(User user) {
+        user.setDateCreated(LocalDateTime.now());
+        user.setUserType(UserType.USER);
+        user.setUsername(user.getEmail());
+        registrationService.register(user);
+        return "redirect:/register";
     }
 }
