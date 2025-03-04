0476dd6fdc116fb231001081febc890ac3220849
Saul Echeverri <shecheve@bancolombia.com.co>
2025-03-04 12:19:46 -0500

    ✨(feat): Backend level validation with Javax Validation - Add the unit to make validations at the backend level - Create the Userdto class to make validations - Create validations at class attributes - Send to save the user

--- src/main/java/edu/study/ecommerce/infrastructure/controller/RegistrationController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/RegistrationController.java
@@ -1,24 +1,31 @@
 package edu.study.ecommerce.infrastructure.controller;
 
 import edu.study.ecommerce.application.service.RegistrationService;
-import edu.study.ecommerce.domain.User;
-import edu.study.ecommerce.domain.UserType;
+import edu.study.ecommerce.infrastructure.dto.UserDTO;
+import edu.study.ecommerce.infrastructure.mapper.UserDTOMapper;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
 
-import java.time.LocalDateTime;
-
+@Slf4j
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
 
 
     private final RegistrationService registrationService;
+    private final View error;
+    private final UserDTOMapper userDTOMapper;
 
-    public RegistrationController(RegistrationService registrationService) {
+    public RegistrationController(RegistrationService registrationService, View error, UserDTOMapper userDTOMapper) {
         this.registrationService = registrationService;
+        this.error = error;
+        this.userDTOMapper = userDTOMapper;
     }
 
 
@@ -34,18 +41,17 @@ public class RegistrationController {
 
     /**
      * Handles POST requests for the registration page.
-     * This method is called when a user submits the registration form.
-     * It creates a new user object and delegates the registration to the registration service.
      *
-     * @param user the user object to be registered. Must not be null.
+     * @param userDTO The user to be registered.
+     * @param bindingResult The result of the validation.
      * @return The name of the view "register".
      */
     @PostMapping
-    public String registerUser(User user) {
-        user.setDateCreated(LocalDateTime.now());
-        user.setUserType(UserType.USER);
-        user.setUsername(user.getEmail());
-        registrationService.register(user);
+    public String registerUser(@Valid UserDTO userDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(e -> log.error("Errors {}", e.getDefaultMessage()));
+        }
+        registrationService.register(userDTOMapper.fromUserDtoToUser(userDTO));
         return "redirect:/register";
     }
 }
--- src/main/java/edu/study/ecommerce/infrastructure/dto/UserDTO.java
+++ src/main/java/edu/study/ecommerce/infrastructure/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package edu.study.ecommerce.infrastructure.dto;
 
 import edu.study.ecommerce.domain.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -15,11 +17,23 @@ public class UserDTO {
 
     Integer id;
     String username;
+
+    @NotBlank(message = "Nombre es requerido")
     String firstName;
+
+    @NotBlank(message = "Apellido es requerido")
     String lastName;
+
+    @Email(message = "Debe de ingresar un email válido")
     String email;
+
+    @NotBlank(message = "Dirección es requerido")
     String address;
+
+    @NotBlank(message = "Celular es requerido")
     String cellphone;
+
+    @NotBlank(message = "Clave es requerido")
     String password;
 
     UserType userType;
