9c3dc2ec1daed8086c13289c10f719b44070933d
Saul Echeverri <shecheve@bancolombia.com.co>
2025-03-06 15:15:15 -0500

    ✨(feat): Temporary authentication and session management - Create the access method to handle the user session - Check the elements from the login form: Username and Password - Implement user search and user session allocation

--- src/main/java/edu/study/ecommerce/infrastructure/controller/LoginController.java
+++ src/main/java/edu/study/ecommerce/infrastructure/controller/LoginController.java
@@ -1,12 +1,24 @@
 package edu.study.ecommerce.infrastructure.controller;
 
+import edu.study.ecommerce.application.service.LoginService;
+import edu.study.ecommerce.infrastructure.dto.UserDTO;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
 
     /**
@@ -16,5 +28,31 @@ public class LoginController {
     @GetMapping
     public String login() {
         return "login";
+    }
+
+
+    /**
+     * Handles POST requests for user login.
+     * This method authenticates the user and redirects them based on their role (ADMIN or regular user).
+     * If the user does not exist, it redirects to the home page.
+     *
+     * @param userDTO the UserDTO object containing the user's credentials (username and password).
+     * @param httpSession the HTTP session to store user-specific data (e.g., user ID).
+     * @return a redirect to the admin page if the user is an ADMIN, or to the home page otherwise.
+     */
+    @PostMapping
+    public String access(UserDTO userDTO, HttpSession httpSession) {
+        userDTO.setEmail(userDTO.getUsername());
+        log.info("UserDTO email: {}", userDTO.getEmail());
+        log.info("UserDTO password: {}", userDTO.getPassword());
+        if (loginService.existUser(userDTO)) {
+            httpSession.setAttribute("iduser", loginService.getUserId(userDTO.getEmail()));
+            if (loginService.getUserType(userDTO).name().equals("ADMIN")) {
+                return "redirect:/admin";
+            } else {
+                return "redirect:/home";
+            }
+        }
+        return "redirect:/home";
     }
 }
