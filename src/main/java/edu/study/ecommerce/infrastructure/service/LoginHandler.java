6beb839db8d9b7938d86e06943dd926c688bc145
Saul Echeverri <shecheve@bancolombia.com.co>
2025-03-17 09:54:49 -0500

    ✨(feat): Improve navigability and the logging user interface

--- src/main/java/edu/study/ecommerce/infrastructure/service/LoginHandler.java
+++ src/main/java/edu/study/ecommerce/infrastructure/service/LoginHandler.java
@@ -14,6 +14,18 @@ import java.util.concurrent.atomic.AtomicReference;
 @Component
 public class LoginHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    /**
+     * Redirects the user to the appropriate page based on their role.
+     *
+     * This method is called when the user successfully logs in.
+     * It retrieves the user's role and redirects them to the appropriate page.
+     *
+     * @param request the HTTP request
+     * @param response the HTTP response
+     * @param authentication the authentication object containing the user's details
+     * @throws IOException if an I/O error occurs
+     * @throws ServletException if a servlet error occurs
+     */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         AtomicReference<String> redirectURL = new AtomicReference<>(request.getContextPath());
